f558ea55578c63f26b87b99160049cbe462bc1fc
Dionleno <contato.dionleno@gmail.com>
2023-10-07 14:05:05 -0300

    renamed task table columns.

--- backend/src/main/resources/db/migration/V2__create-table-tasks.sql
+++ backend/src/main/resources/db/migration/V2__create-table-tasks.sql
@@ -2,9 +2,9 @@ CREATE TABLE tasks (
     id uuid NOT NULL,
     name varchar(255) NOT NULL,
     status smallint,
-    dateStart timestamp(6) without time zone,
-    dateEnd timestamp(6) without time zone,
-    userID uuid,
+    date_start timestamp(6) without time zone,
+    date_end timestamp(6) without time zone,
+    user_id uuid,
     PRIMARY KEY (id),
-    FOREIGN KEY (userID) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
