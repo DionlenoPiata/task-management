d46a2fdf06462edfb8f14c4038da258acd1a40cf
Dionleno <contato.dionleno@gmail.com>
2023-10-07 14:10:01 -0300

    Adding getters and setters.

--- backend/src/main/java/com/example/backend/models/TaskModel.java
+++ backend/src/main/java/com/example/backend/models/TaskModel.java
@@ -16,5 +16,53 @@ public class TaskModel {
     private TaskStatus status;
     private Date dateStart;
     private Date dateEnd;
-    private UUID userID;
+    private UUID userId;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public Date getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(Date dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserID(UUID userId) {
+        this.userId = userId;
+    }
 }
