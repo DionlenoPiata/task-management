c7a8ee698fb9d56194507d6fbd0692b12a19f5ee
Dionleno <contato.dionleno@gmail.com>
2023-10-07 14:34:28 -0300

    Adding sql file to populate users table.

---
+++ backend/src/main/resources/db/migration/V3__create-users-populate.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, name)
+VALUES('745effa1-f939-4d9f-9af2-30075d52494c', 'João');
+
+INSERT INTO users (id, name)
+VALUES('32c6ac69-79fa-4e87-9f5d-4734dd94fe5c', 'Maria');
+
+INSERT INTO users (id, name)
+VALUES('917c90d0-59dd-437d-8e4c-06b860053949', 'Jose');
