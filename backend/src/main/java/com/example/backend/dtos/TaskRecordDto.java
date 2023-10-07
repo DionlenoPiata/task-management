f5cd3794321bb5ffedfcfffb186613b9961a4c33
Dionleno <contato.dionleno@gmail.com>
2023-10-07 11:08:05 -0300

    Adding task DTO.

---
+++ backend/src/main/java/com/example/backend/dtos/TaskRecordDto.java
@@ -0,0 +1,12 @@
+package com.example.backend.dtos;
+
+import com.example.backend.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record TaskRecordDto(@NotBlank String name, TaskStatus status, Date dateStart, Date dateEnd,
+                            UUID userID) {
+}
