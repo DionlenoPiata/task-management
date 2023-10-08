efe351bff1b87b9c38866c1db28aca1dedef59ce
Dionleno <contato.dionleno@gmail.com>
2023-10-08 10:42:33 -0300

    Adding foreign key task in table tasks.

--- backend/src/main/resources/db/migration/V2__create-table-tasks.sql
+++ backend/src/main/resources/db/migration/V2__create-table-tasks.sql
@@ -2,9 +2,11 @@ CREATE TABLE tasks (
     id uuid NOT NULL,
     name varchar(255) NOT NULL,
     status smallint,
-    date_start timestamp(6) without time zone,
-    date_end timestamp(6) without time zone,
+    start_date timestamp(6) without time zone,
+    end_date timestamp(6) without time zone,
     user_id uuid,
+    task_id uuid,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
