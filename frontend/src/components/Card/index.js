c9e9b769407f0f57a266ab4bbeda874073413c62
Dionleno <contato.dionleno@gmail.com>
2023-10-09 11:54:10 -0300

    Adding buttons edit and delete in card.

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,4 +1,8 @@
 import { Draggable } from "react-beautiful-dnd";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { Avatar, CardHeader, Author, CardFooter, DragItem } from "./styles";
 
 function Card({ item, index }) {
@@ -12,13 +16,28 @@ function Card({ item, index }) {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
           >
-            <CardHeader>Deploy aplicação</CardHeader>
+            <Stack direction="row" justifyContent="space-between" spacing={0}>
+              <CardHeader>Deploy aplicação</CardHeader>
+              <Stack direction="row">
+                <IconButton aria-label="delete">
+                  <EditIcon />
+                </IconButton>
+                <IconButton aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+
             <span>
               Inicia em: 10-10-2023 <br />
               Prazo finalizar: 12/10/2023
             </span>
 
-            <span>SubTarefas</span>
+            <span>
+              SubTarefas
+              <br />- Configurar docker
+              <br />- Configurar Nginx
+            </span>
             <CardFooter>
               <span>{"#47fh4d"}</span>
               <Author>
