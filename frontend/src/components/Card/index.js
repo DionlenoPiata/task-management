6709b2785d2b154468650fda1d89664ed280213e
Dionleno <contato.dionleno@gmail.com>
2023-10-10 08:09:58 -0300

    Adding user in card.

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import axios from "axios";
 import { Avatar, CardHeader, Author, CardFooter, DragItem } from "./styles";
 
 function Card({ index, item, subItems }) {
   const subTaks = subItems.filter((el) => el.taskId === item.id);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    async function makeRequest() {
+      try {
+        const response = await axios.request({
+          method: "get",
+          url: `http://localhost:8080/users/${item.userId}`,
+          headers: {},
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (item.userId) {
+      makeRequest();
+    }
+  };
 
   return (
     <Draggable draggableId={item.id} index={index}>
@@ -53,7 +78,7 @@ function Card({ index, item, subItems }) {
             <CardFooter>
               <span>{`#${item.id.substr(0, 5)}`}</span>
               <Author>
-                {"Dionleno"}
+                {user && user.name}
                 <Avatar
                   src={`https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png`}
                 />
